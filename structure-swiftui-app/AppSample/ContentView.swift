7e6ea9fa652ec3ffb0623c86a4a20b3b022512fc
Eva Ury <eva.ury@fulbrightmail.org>
2024-12-12 18:11:23 -0500

    changes styles in ContentView for chapter 1 experiments

--- structure-swiftui-app/AppSample/ContentView.swift
+++ structure-swiftui-app/AppSample/ContentView.swift
@@ -6,12 +6,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        HStack (spacing: 30){
+            Image(systemName: "book.fill")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.yellow)
+                .background(Color.black)
+                .cornerRadius(8)
+            Text("Dear, Reader")
+                .foregroundColor(.black)
+//                .padding([.bottom, .trailing], 10)
+                .font(.callout)
+            Image(systemName: "book.fill")
+                .imageScale(.large)
+                .foregroundColor(.yellow)
+                .background(Color.black)
+                .cornerRadius(8)
         }
-        .padding()
+        .padding(10)
+        .background(Color(red: 34/255, green: 139/255, blue: 80/255))
+        .cornerRadius(5)
     }
 }
